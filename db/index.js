7ce428fd910e9f09b0d961ec3c871f08a9bb8deb
ogberagabriel234@gmail.com <ogberagabriel234@gmail.com>
2023-08-11 17:41:56 +0100

    Added Mongo Atlas VS 1.0

--- db/index.js
+++ db/index.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
 const MONGO_URI =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/Tea-Explorer";
+"mongodb+srv://Gabriel:6IKVRbqusGKvLIew@cluster0.gses18a.mongodb.net/?retryWrites=true&w=majority"
 
 mongoose
   .connect(MONGO_URI)
@@ -14,6 +14,8 @@ mongoose
     const databaseName = x.connections[0].name;
     console.log(`Connected to Mongo! Database name: "${databaseName}"`);
   })
-  .catch((err) => {
+  .catch((err) => { 
     console.error("Error connecting to mongo: ", err);
   });
+
+ 
