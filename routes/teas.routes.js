cb502791b98833140f867c797d767798bf150743
ogberagabriel234@gmail.com <ogberagabriel234@gmail.com>
2023-08-14 16:50:09 +0100

    Created The Tea Review

--- routes/teas.routes.js
+++ routes/teas.routes.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const Tea = require('../models/Tea.model.js')
 const isLoggedOut = require("../middleware/isLoggedOut");
 const isLoggedIn = require("../middleware/isLoggedIn");
+const ReviewModel = require("../models/Review.model.js");
+const User = require("../models/User.model.js");
 
 
 router.get('/teas', isLoggedIn, async (req, res) => {
@@ -18,13 +20,50 @@ router.get('/teas/:teaId', isLoggedIn, async(req, res) =>{
     try {
         const {teaId} = req.params
         const teasDetail = await Tea.findById(teaId)
-        console.log(teasDetail)
+        //console.log(teasDetail)
+
+        const users = await User.find()
+
+        await teasDetail.populate('review')
+
+        await teasDetail.populate({
+            path: 'review',
+            populate: {
+                path: 'author',
+                model: 'User'
+            }
+        })
+
+        //console.log(teasDetail)
 
         res.render('teas/teas-details.hbs', {teas: teasDetail})
     } catch (error) {
         console.log(error)
     }
 })
+
+router.post('/review/create/:teaId', isLoggedIn, async (req, res) => {
+    try {
+        const { teaId } = req.params;
+        const { content, author } = req.body;
+
+        // Create a new review
+        const review = await ReviewModel.create({ content });
+
+        // Get the details of the currently logged-in user
+        const user = req.session.currentUser;
+        console.log(user);
+
+        // Update the tea's review array and the user's review array
+        const teaUpdate = await Tea.findByIdAndUpdate(teaId, { $push: { review: review._id } });
+        const userUpdate = await User.findByIdAndUpdate(user._id, { $push: { review: review._id } });
+
+        // Redirect back to the tea details page
+        res.redirect(`/teas/${teaId}`);
+    } catch (error) {
+        console.log(error);
+    }
+});
 
 
 
