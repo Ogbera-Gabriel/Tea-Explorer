317551e59995c26aee8ea369887cf57f9106cc7f
jsguillotte <julia.guillotte@gmail.com>
2023-08-14 04:53:40 -0500

    add Tea Schema

---
+++ models/Tea.model.js
@@ -0,0 +1,21 @@
+const {Schema, model} = require('mongoose')
+
+const teaSchema = new Schema (
+    { name: String,
+      image: String,
+      origin: String,
+      description: String,
+      caffeine: String,
+      tasteDescription: String,
+      review: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Review'
+      }]
+
+    },
+    {
+        timestamps: true
+    }
+
+)
+module.exports = model('Tea', teaSchema)
