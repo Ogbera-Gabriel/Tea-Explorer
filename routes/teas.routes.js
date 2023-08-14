4252a4fe3ebce627d414f4c649328c4839fe6327
ogberagabriel234@gmail.com <ogberagabriel234@gmail.com>
2023-08-14 15:07:58 +0100

    tea-details

--- routes/teas.routes.js
+++ routes/teas.routes.js
@@ -2,15 +2,30 @@ const router = require("express").Router();
 const Tea = require('../models/Tea.model.js')
 const isLoggedOut = require("../middleware/isLoggedOut");
 const isLoggedIn = require("../middleware/isLoggedIn");
+
+
 router.get('/teas', isLoggedIn, async (req, res) => {
     try{
         let allTeasFromDB = await Tea.find()
-        console.log(allTeasFromDB)
         res.render('teas/teas.hbs', {teas: allTeasFromDB})
     }
     catch(error){
         console.log(error)
     }
 })
+
+router.get('/teas/:teaId', isLoggedIn, async(req, res) =>{
+    try {
+        const {teaId} = req.params
+        const teasDetail = await Tea.findById(teaId)
+        console.log(teasDetail)
+
+        res.render('teas/teas-details.hbs', {teas: teasDetail})
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
 
 module.exports = router;
