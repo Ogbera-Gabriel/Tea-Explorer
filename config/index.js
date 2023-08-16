f421be9e2e9a475652de51640cb9a84f654846e5
ogberagabriel234@gmail.com <ogberagabriel234@gmail.com>
2023-08-16 16:32:16 +0100

    Change Mongo-Link

--- config/index.js
+++ config/index.js
@@ -27,7 +27,7 @@ const MongoStore = require("connect-mongo");
 
 // Connects the mongo uri to maintain the same naming structure
 const MONGO_URI =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/Tea-Explorer";
+"mongodb+srv://Gabriel:6IKVRbqusGKvLIew@cluster0.gses18a.mongodb.net/Tea";
 
 // Middleware configuration
 module.exports = (app) => {
